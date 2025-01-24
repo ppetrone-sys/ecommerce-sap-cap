fee8c2defb16e120f8cbb95e4109e92381b2fbc5
ppetrone-syscons <pietro.petrone@sysconsgroup.com>
2025-01-24 13:59:14 +0100

    feat: expose new roles like sale manager - inventory manager - store supervisors in AuthorizationService

--- srv/AuthorizationService.java
+++ srv/AuthorizationService.java
@@ -7,15 +7,33 @@ import java.util.Map;
 public class AuthorizationService extends BaseService {
 
     private static final String ADMIN = "Admin";
+    private static final String INVENTORY_MANAGER = "InventoryManager";
+    private static final String SALES_MANAGER = "SalesManager";
+    private static final String STORE_SUPERVISOR = "StoreSupervisor";
 
     @Override
     public void init() {
         this.on("isAdmin", this::isAdmin);
+        this.on("InventoryManager", this::isAdmin);
+        this.on("SalesManager", this::isAdmin);
+        this.on("StoreSupervisor", this::isAdmin);
         this.on("userRoles", this::getUserRoles);
     }
 
     public boolean isAdmin(Request req) {
         return req.getUser().is(ADMIN);
+    }
+
+    public boolean isInventoryManager(Request req) {
+        return req.getUser().is(INVENTORY_MANAGER);
+    }
+
+    public boolean isSalesManager(Request req) {
+        return req.getUser().is(SALES_MANAGER);
+    }
+
+    public boolean isStoreSupervisor(Request req) {
+        return req.getUser().is(STORE_SUPERVISOR);
     }
 
     public List<String> getUserRoles(Request req) {
--- srv/authorization-service.cds
+++ srv/authorization-service.cds
@@ -1,6 +1,12 @@
 service AuthorizationService {
 
   function isAdmin() returns  Boolean;
+  
+  function InventoryManager() returns  Boolean;
+  
+  function SalesManager() returns  Boolean;
+  
+  function StoreSupervisor() returns  Boolean;
 
   function userRoles() returns Array of String;
 }
